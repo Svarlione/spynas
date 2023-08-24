215a17bdc162c0841ed3bfb2e8468cebe030399f
Svarlione <ruslan.brigockij@gmail.com>
2023-08-24 19:38:49 +0300

    Add project files.

---
+++ spynas/Program.cs
@@ -0,0 +1,13 @@
+﻿string myPass = "Russ";
+string userPass = "Melon";
+string hackPass = "00011100011";
+Console.WriteLine("Hello, please enter password");
+string Pass = Console.ReadLine();
+if  (Pass == myPass)
+    Console.WriteLine("Sveiki, jus sekmingai prisjunget");
+else if (Pass == userPass)
+    Console.WriteLine("Sveiki, jus sekmingai prisijunget");
+else if(Pass == hackPass)
+    Console.WriteLine("!!!!!NILAUZTA!!!!");
+else
+    Console.WriteLine("Bandykit dar karta");
